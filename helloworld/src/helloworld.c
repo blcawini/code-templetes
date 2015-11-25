59790167a4cff895ebd39d6fea64a6a4d2e0c6ba
blca <blca@snowi>
2015-11-26 00:00:25 +0900

    test

---
+++ helloworld/src/helloworld.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : helloworld.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+
+
+	return EXIT_SUCCESS;
+}
